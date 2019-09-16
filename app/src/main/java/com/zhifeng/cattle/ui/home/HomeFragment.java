81b3fd2eca6c5914ba87e45a4f8993373b5c8c85
ligenghuang <1045105946@qq.com>
2019-09-16 23:13:02 +0800

    首页

--- app/src/main/java/com/zhifeng/cattle/ui/home/HomeFragment.java
+++ app/src/main/java/com/zhifeng/cattle/ui/home/HomeFragment.java
@@ -1,7 +1,17 @@
 package com.zhifeng.cattle.ui.home;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhifeng.cattle.R;
 import com.zhifeng.cattle.actions.BaseAction;
 import com.zhifeng.cattle.utils.base.UserBaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
   *
@@ -13,13 +23,39 @@ import com.zhifeng.cattle.utils.base.UserBaseFragment;
  */
 
 public class HomeFragment extends UserBaseFragment {
+    View view;
+    @BindView(R.id.top_view)
+    View topView;
+
     @Override
     protected BaseAction initAction() {
         return null;
     }
 
     @Override
-    protected void initialize() {
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mContext = getContext();
+        mActivity = activity;
+    }
 
+    @Override
+    protected void initialize() {
+        init();
+        loadView();
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        binding();
+        mImmersionBar.setStatusBarView(getActivity(), topView);
+        return view;
     }
 }
